480f1cd002ef6fe3c1ba77020c053dbc897ab79f
Roman Voitalik <voitalik1@gmail.com>
2022-11-04 00:05:28 +0200

    todo

--- src/main/java/mate/academy/spring/Main.java
+++ src/main/java/mate/academy/spring/Main.java
@@ -2,6 +2,6 @@ package mate.academy.spring;
 
 public class Main {
     public static void main(String[] args) {
-
+        // todo configure ApplicationContext , DataSource, AppConfig class
     }
 }
